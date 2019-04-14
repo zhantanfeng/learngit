47e8b72f6a48b3056fef94329e764915f4853e7c
df <1148705128@qq.com>
2019-04-14 20:40:42 +0800

    上传测试

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template,request
+from flasgger import Swagger
+from nameko.standalone.rpc import ClusterRpcProxy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "dfdfdffdad"
+Swagger(app)
+CONFIG = {'AMQP_URI': "amqp://guest:guest@localhost"}
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/compute',methods=['GET','POST'])
+def compute():
+    if request.method == 'POST':
+        schoolName = request.form.get('schoolName')
+        institutionName = request.form.get('institutionName')
+    with ClusterRpcProxy(CONFIG) as rpc:
+        institution_Id = rpc.test.get_institutionId(schoolName,institutionName)
+        try:
+            teacher_name = rpc.test.get_academicianName(institution_Id[0])
+            teacher_name1 = []
+            for i in teacher_name:
+                teacher_name1.append(i[0].lstrip("['").rstrip("']"))
+            print(teacher_name1)
+        except BaseException as e:
+            teacher_name1 = []
+        return render_template("outcome.html", teacher_name = teacher_name1)
+
+@app.route('/test',methods=['GET','POST'])
+def test():
+    with ClusterRpcProxy(CONFIG) as rpc:
+        t = rpc.test.test()
+        print(t)
+    return render_template("a.html",t = t)
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.run(host='0.0.0.0' ,debug=True)
